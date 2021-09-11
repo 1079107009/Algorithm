58e9dcf00c7feab3d09fd95947ea85284506a57a
linke <lin_ke@hunliji.com>
2021-09-11 16:51:55 +0800

    顺时针打印矩阵

--- app/src/main/java/com/example/algorithm/Algorithm.kt
+++ app/src/main/java/com/example/algorithm/Algorithm.kt
@@ -9,6 +9,49 @@ fun main() {
 object Algorithm {
 
     /**
+     * 一层一层从外到里打印，观察可知每一层打印都有相同的处理步骤，唯一不同的是上下左右的边界不同了。
+     * 因此使用四个变量 r1, r2, c1, c2 分别存储上下左右边界值，从而定义当前最外层。
+     * 打印当前最外层的顺序：
+     * 从左到右打印最上一行->从上到下打印最右一行->从右到左打印最下一行->从下到上打印最左一行。
+     * 应当注意只有在 r1 != r2 时才打印最下一行，也就是在当前最外层的行数大于 1 时才打印最下一行，
+     * 这是因为当前最外层只有一行时，继续打印最下一行，会导致重复打印。打印最左一行也要做同样处理。
+     */
+    fun printMatrix(matrix: Array<IntArray>): List<Int> {
+        val result = arrayListOf<Int>()
+        var r1 = 0
+        var r2: Int = matrix.size - 1
+        var c1 = 0
+        var c2: Int = matrix[0].size - 1
+        while (r1 <= r2 && c1 <= c2) {
+            // 上
+            for (i in c1..c2) {
+                result.add(matrix[r1][i])
+            }
+            // 右
+            for (i in r1 + 1..r2) {
+                result.add(matrix[i][c2])
+            }
+            // 下
+            if (r1 != r2) {
+                for (i in c2 - 1 downTo c1) {
+                    result.add(matrix[r2][i])
+                }
+            }
+            // 左
+            if (c1 != c2) {
+                for (i in r2 - 1 downTo r1 + 1) {
+                    result.add(matrix[i][c1])
+                }
+            }
+            r1++
+            r2--
+            c1++
+            c2--
+        }
+        return result
+    }
+
+    /**
      * 要求时间复杂度 O(N)，空间复杂度 O(1)。因此不能使用排序的方法，也不能使用额外的标记数组。
      *
      * 对于这种数组元素在 [0, n-1] 范围内的问题，可以将值为 i 的元素调整到第 i 个位置上进行求解。
