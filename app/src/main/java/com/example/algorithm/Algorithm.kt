4c5891b33576327b450995ffc253b8204c827946
lipin <1079107009@qq.com>
2021-10-09 09:15:24 +0800

    整数反转

--- app/src/main/java/com/example/algorithm/Algorithm.kt
+++ app/src/main/java/com/example/algorithm/Algorithm.kt
@@ -9,6 +9,25 @@ fun main() {
 object Algorithm {
 
     /**
+     * 给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+     * 如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
+     * 假设环境不允许存储 64 位整数（有符号或无符号）。
+     */
+    fun reverse(x: Int): Int {
+        var temp = x
+        var rev = 0
+        while (temp != 0) {
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0
+            }
+            val digit = temp % 10
+            temp /= 10
+            rev = rev * 10 + digit
+        }
+        return rev
+    }
+
+    /**
      * 一层一层从外到里打印，观察可知每一层打印都有相同的处理步骤，唯一不同的是上下左右的边界不同了。
      * 因此使用四个变量 r1, r2, c1, c2 分别存储上下左右边界值，从而定义当前最外层。
      * 打印当前最外层的顺序：
