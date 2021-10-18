c0d523c0361ed8ff76d2ba160d9d5275ffda8f4d
lipin <1079107009@qq.com>
2021-10-18 10:09:04 +0800

    合并两个有序链表

--- app/src/main/java/com/example/algorithm/Algorithm.kt
+++ app/src/main/java/com/example/algorithm/Algorithm.kt
@@ -12,6 +12,23 @@ fun main() {
 object Algorithm {
 
     /**
+     * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+     */
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        if (l1 == null) {
+            return l2
+        } else if (l2 == null) {
+            return l1
+        } else if (l1.`val` < l2.`val`) {
+            l1.next = mergeTwoLists(l1.next, l2)
+            return l1
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next)
+            return l2
+        }
+    }
+
+    /**
      * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效
      * 有效字符串需满足：
      * 左括号必须用相同类型的右括号闭合。
