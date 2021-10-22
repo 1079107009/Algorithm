758d8f221d5cf9cc8acf71cc955b12f99a14dc1f
lipin <1079107009@qq.com>
2021-10-22 09:31:10 +0800

    删除有序数组中的重复项

--- app/src/main/java/com/example/algorithm/Algorithm.kt
+++ app/src/main/java/com/example/algorithm/Algorithm.kt
@@ -12,6 +12,28 @@ fun main() {
 object Algorithm {
 
     /**
+     * 删除有序数组中的重复项
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     */
+    fun removeDuplicates(nums: IntArray): Int {
+        val n = nums.size
+        if (n == 0) {
+            return 0
+        }
+        var fast = 1
+        var slow = 1
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast]
+                ++slow
+            }
+            ++fast
+        }
+        return slow
+    }
+
+    /**
      * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
      * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
      * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
