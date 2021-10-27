e226615a0b4d7deacabb383f6ec8fb75f6842690
lipin <1079107009@qq.com>
2021-10-27 09:47:14 +0800

    搜索插入位置

--- app/src/main/java/com/example/algorithm/Algorithm.kt
+++ app/src/main/java/com/example/algorithm/Algorithm.kt
@@ -1,15 +1,48 @@
 package com.example.algorithm
 
-import com.example.algorithm.Algorithm.duplicate
+import com.example.algorithm.Algorithm.searchInsert
 import java.util.*
 import kotlin.collections.HashMap
-import kotlin.math.min
 
 fun main() {
-    duplicate(intArrayOf(2, 3, 1, 0, 2, 5))
+    searchInsert(intArrayOf(1, 3, 5, 6), 2)
 }
 
 object Algorithm {
+
+    /**
+     * 搜索插入位置
+     *
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+     * 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * 请必须使用时间复杂度为 O(log n) 的算法。
+     */
+    fun searchInsert(nums: IntArray, target: Int): Int {
+        val size = nums.size
+        var left = 0
+        var right = size - 1
+        var ans = size
+        while (left <= right) {
+            val mid = ((right - left) / 2) + left
+            if (target <= nums[mid]) {
+                right = mid - 1
+                ans = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return ans
+    }
+
+    /**
+     * 实现strStr()函数。
+     *
+     * 给你两个字符串haystack和needle，请你在haystack字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。
+     * 如果不存在，则返回-1 。
+     */
+    fun strStr(haystack: String, needle: String): Int {
+        return haystack.indexOf(needle)
+    }
 
     /**
      * 删除有序数组中的重复项
